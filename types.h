afdc936e2996c0822add4b26ba18b36e57156aac
elmarsan <mselias97@gmail.com>
2024-12-08 19:28:42 +0100

    Add more types

--- types.h
+++ types.h
@@ -7,5 +7,12 @@ typedef short int s16;
 typedef unsigned short int u16;
 typedef int s32;
 typedef unsigned int u32;
+typedef signed long long s64;
+typedef unsigned long long u64;
+
+#define BIT(x) 1 << (x)
+#define KB(x) ((u64)1024 * x)
+#define MB(x) ((u64)1024 * KB(x))
+#define GB(x) ((u64)1024 * MB(x))
 
 #endif
